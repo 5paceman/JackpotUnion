5cf789d86a5e78837b4154d55203810076ea2e20
5paceman <tom.james15@hotmail.com>
2023-08-13 23:17:53 +0100

    fixed latest result not pulled from draw_date

--- app/Providers/LotteryResultsProvider.php
+++ app/Providers/LotteryResultsProvider.php
@@ -25,12 +25,12 @@ class LotteryResultsProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::directive('lottoresult', function(string $expression) {
-            $lottoResult = LotteryResult::latest()->first();
+            $lottoResult = LotteryResult::all()->sortByDesc('draw_date')->first();
             return $lottoResult ? $lottoResult->formattedResults() : 'Unable to get results';
         });
 
         Blade::directive('drawdate', function(string $expression) {
-            $lottoResult = LotteryResult::latest()->first();
+            $lottoResult = LotteryResult::all()->sortByDesc('draw_date')->first();
             return $lottoResult ? date_create($lottoResult->draw_date)->format('d-m-Y') : 'Unable to get draw date';
         });
     }
