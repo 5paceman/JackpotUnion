e9bd5294105d20b745ba834c82236282625ca0fb
5paceman <tom.james15@hotmail.com>
2023-08-13 08:38:56 +0100

    added AWS EB config files

---
+++ .platform/hooks/prebuild/install_certbot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo python3 -m venv /opt/certbot
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot certbot-nginx
+sudo ln -sf /opt/certbot/bin/certbot /usr/bin/certbot
