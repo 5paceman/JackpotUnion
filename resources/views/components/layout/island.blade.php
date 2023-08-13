852c54ae5ee6f7ceb9f6122f8d21413eb0c8e588
5paceman <tom.james15@hotmail.com>
2023-08-13 08:37:25 +0100

    AWS compatible and favicons

--- resources/views/components/layout/island.blade.php
+++ resources/views/components/layout/island.blade.php
@@ -3,8 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/teamlotto.css" />
+        <link rel="stylesheet" href="{{ asset('css/teamlotto.css') }}" />
         <link href="https://db.onlinewebfonts.com/c/29aaa00c8f322b47863a40b19b7e9e6b?family=Effra+Heavy" rel="stylesheet"> 
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
         <title>{{ config('app.name') }}</title>
     </head>
     <body class="flex-center padding-4">
