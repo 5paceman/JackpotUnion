e3e985caff96685eba296cd7f403066755a9a571
5paceman <tom.james15@hotmail.com>
2023-08-14 18:24:15 +0100

    fixed scheduler

--- .platform/hooks/postdeploy/configure_scheduler.sh
+++ .platform/hooks/postdeploy/configure_scheduler.sh
@@ -1,3 +1,4 @@
-#!\bin\sh
+#!/bin/sh
 
-echo "* * * * * root cd /var/app/current && php artisan schedule:run >> /dev/null 2>&1" | sudo tee /etc/cron.d/laravel_scheduler_cron
+echo "* * * * * root cd /var/app/current && php artisan schedule:run >> /dev/null 2>&1" \
+    | sudo tee /etc/cron.d/laravel_scheduler_cron
